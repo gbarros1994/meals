24710c1769ed01ae7598f5c46902efa24e424ed8
Gerson Barros <55881896+gbarros1994@users.noreply.github.com>
2022-04-19 23:45:00 -0300

    category

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+
+import 'screens/categories_screen.dart';
  
 void main() => runApp(MyApp());
  
@@ -10,7 +12,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      home: CategoriesScreen(),
     );
   }
 }
