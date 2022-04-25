72d4160ec85a2a0269a0e8ff3fda1d229ee4690c
Gerson Barros <55881896+gbarros1994@users.noreply.github.com>
2022-04-24 22:41:50 -0300

    pass date

--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
+import '../models/category.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
-  const CategoriesMealsScreen({ Key? key }) : super(key: key);
+  
+  final Category category;
+
+  CategoriesMealsScreen(this.category);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Receitas'),
+        title: Text(category.title),
       ),
       body: Center(
-        child: Text('Receitas por Categoria')
+        child: Text('Receitas por Categoria ${category.id}')
       ),
     );
   }
