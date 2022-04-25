484fa5bcb0296f99b0affb6eb62ccc22053736be
Gerson Barros <55881896+gbarros1994@users.noreply.github.com>
2022-04-25 00:11:11 -0300

    new components

---
+++ lib/data/stores_data.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import '../models/store.dart';
+ 
+// ignore: constant_identifier_names
+const STORES_LIST = [
+  Store(
+    id: 's1',
+    name: 'Mc Donals',
+    address: 'Rua Barcelona'
+  ),
+  Store(
+    id: 's2',
+    name: 'Burguer Kink',
+    address: 'Rua Nova Jerusa'
+  )
+];
---
+++ lib/models/store.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Store {
+  final String id;
+  final String name;
+  final String address;
+
+  const Store({
+    required this.id,
+    required this.name,
+    required this.address,
+  });
+}
