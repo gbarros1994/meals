fd170f030c2f5c69222b5307c6500208517933d0
Gerson Barros <55881896+gbarros1994@users.noreply.github.com>
2022-05-08 19:03:39 -0300

    positioned

--- lib/components/meal_item.dart
+++ lib/components/meal_item.dart
@@ -34,6 +34,27 @@ class MealItem extends StatelessWidget {
                     width: double.infinity,
                     fit: BoxFit.cover
                   ),
+                ),
+                Positioned(
+                  bottom: 20,
+                  right: 10,
+                  child: Container(
+                    width: 300,
+                    color: Colors.black54,
+                    padding: EdgeInsets.symmetric(
+                      vertical: 5,
+                      horizontal: 20,
+                    ),
+                    child: Text(
+                      meal.title,
+                      style: TextStyle(
+                        fontSize: 26,
+                        color: Colors.white
+                      ),
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
                 )
               ],
             )
