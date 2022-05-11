e7101bf57d97d05d1a0ea0c4658580b5ec7c0a8e
Gerson Barros <55881896+gbarros1994@users.noreply.github.com>
2022-05-10 23:30:50 -0300

    add containers

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -29,6 +29,31 @@ class MealDetailScreen extends StatelessWidget {
                 'Ingredientes',
                 style: Theme.of(context).textTheme.titleLarge,
               ),
+            ),
+            Container(
+              width: 300,
+              height: 250,
+              padding: EdgeInsets.all(10),
+              margin: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(color: Colors.grey),
+                borderRadius: BorderRadius.circular(10)
+              ),
+              child: ListView.builder(
+                itemCount: meal.ingredients.length,
+                itemBuilder: (ctx, index) {
+                  return Card(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 5,
+                        horizontal: 10
+                      ),
+                      child: Text(meal.ingredients[index]),
+                    ),
+                    color: Theme.of(context).accentColor,
+                  );
+                }),
             )
           ],
         )
