fdbc8e51fd2cac58e30d7220621c9b60dc8d1870
Gerson Barros <55881896+gbarros1994@users.noreply.github.com>
2022-05-11 23:34:32 -0300

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:meals/screens/meal_detail_screen.dart';
 
+import 'screens/tabs_screen.dart';
 import 'screens/categories_screen.dart';
 
 import 'screens/categories_meals_screen.dart';
@@ -27,7 +28,7 @@ class MyApp extends StatelessWidget {
         ),
       ),
       routes: {
-        AppRoutes.HOME: (context) => CategoriesScreen(),
+        AppRoutes.HOME: (context) => TabsScreen(),
         AppRoutes.CATEGORIES_MEALS: (context) => CategoriesMealsScreen(),
         AppRoutes.MEAL_DETAIL: (context) => MealDetailScreen(),
       },
